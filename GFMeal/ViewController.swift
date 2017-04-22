4f08d6bb2905736004bb9026e2d70835055fdb3d
Minsoo Park <minsoo1003@gmail.com>
2017-04-22 15:54:03 +0900

    Add login state listener

--- GFMeal/ViewController.swift
+++ GFMeal/ViewController.swift
@@ -13,12 +13,22 @@ import Firebase
 
 class ViewController: UIViewController, LoginButtonDelegate {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = LoginButton(readPermissions: [.email, .publicProfile])
-        loginButton.center = view.center
-        loginButton.delegate = self
-        view.addSubview(loginButton)
+//        let loginButton = LoginButton(readPermissions: [.email, .publicProfile])
+//        loginButton.delegate = self
+//        view.addSubview(loginButton)
+        
+        self.loginButton.addTarget(self, action: #selector(ViewController.facebookLogin), for: .touchUpInside)
+        
+        FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
+            let currentUser = auth.currentUser
+            if (currentUser != nil) {
+                self.performSegue(withIdentifier: "showGroupList", sender: self)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +45,23 @@ class ViewController: UIViewController, LoginButtonDelegate {
     
     func loginButtonDidLogOut(_ loginButton: LoginButton) {
         
+    }
+    
+    func facebookLogin() {
+        let fbLoginManager: LoginManager = LoginManager()
+        fbLoginManager.logIn([.email, .publicProfile], viewController: self) { (result) in
+            self.getFBUserData()
+        }
+    }
+    
+    func getFBUserData(){
+        if ((FBSDKAccessToken.current()) != nil) {
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.performSegue(withIdentifier: "showGroupList", sender: self)
+                }
+            })
+        }
     }
 }
 
